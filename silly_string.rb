4d79e64799cb076fee46b455d397b6b30a0ee970
Eliza Taylor <elizataylor2002@gmail.com>
2018-07-09 19:19:41 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -17,21 +17,21 @@ phrase = "IM NOT SHOUTING"
 
 lowercased_phrase = phrase # Put a . after phrase and use a String method.
 
-puts #{phrase.downcase}
+puts "lowercased_phrase"
 
 # Challenge 4: Count how many letters are in that big_word
 
 big_word = "supercalifragilisticexpialidocious"
 
-letter_count = big_word # Put a . after big_word and use a String method.
+letter_count = big_word.length # Put a . after big_word and use a String method
 
 puts "There are #{letter_count} in #{big_word}"
 
 # Challenge 5: Add "wow" and "mom" to big_word
 
-bigger_word = big_word # Use String concatenation to add "wow" and "mom" to big_word
+bigger_word = big_word + "wow" + "mom" # Use String concatenation to add "wow" and "mom" to big_word
 
-new_letter_count = bigger_word.size
+new_letter_count = bigger_word.length
 
 puts "There are now #{new_letter_count} in #{bigger_word}"
 
